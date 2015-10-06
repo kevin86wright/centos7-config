dffd64338045e910ccccaec4ef6b734f3438e113
Kevin Wright <kevin86wright@gmail.com>
2015-10-06 16:15:07 -0500

    Update join.sh

--- active_directory/join.sh
+++ active_directory/join.sh
@@ -81,7 +81,7 @@ echo $password | realm join --user=$user $domain
 
 # Configuring SSSD
 sed -i "s/use_fully_qualified_names/#use_fully_qualified_names/g" /etc/sssd/sssd.conf
-realm permit --groups linuxadmins linuxusers
+realm permit --groups linuxitadmins linuxusers
 echo $adgroup'  ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 # Ask user to reboot the system
